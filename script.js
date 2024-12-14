f6038ad2aec0527e9a5b880e84aed2358c36022f
Sfiso Ngobese <andilekngobese@gmail.com>
2024-12-15 00:27:55 +0200

    updates on new animations

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting)
+            entry.target.classList.add("show")
+        else
+            entry.target.classList.remove("show")
+
+        console.log(entry)
+    })
+})
+
+const hiddenElement = document.querySelectorAll(".hidden");
+hiddenElement.forEach((el) => observer.observe(el))
